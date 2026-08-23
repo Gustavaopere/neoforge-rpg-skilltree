38c403baa0bab58beba0500d166cfc272fe6f374
Gustavaopere <gustavaopere@gmail.com>
2026-08-23 16:21:17 -0300

    test: run specialization reconciliation regression

--- scripts/test-core.sh
+++ scripts/test-core.sh
@@ -26,3 +26,4 @@ java -cp "$OUT" dev.gustavopere.rpgskilltree.core.SystemFoundationTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.ArchetypeSpecificityScoreTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.MorphEcologyPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.ProgressionStateMigrationMatrixTest
+java -cp "$OUT" dev.gustavopere.rpgskilltree.core.SpecializationReconciliationTest
