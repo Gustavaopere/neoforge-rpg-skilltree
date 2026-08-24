27715464a273cfd300f2e4e91baa33f210ae4105
Gustavaopere <gustavaopere@gmail.com>
2026-08-24 14:56:29 -0300

    test(heavy): run RED heavy consumer policy contract

--- scripts/test-core.sh
+++ scripts/test-core.sh
@@ -34,6 +34,7 @@ java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkRuntimeStateTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkAttackPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkFinalizationPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkTransitionPolicyTest
+java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkHeavyImpactConsumerPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkLifecyclePolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkLifecycleBoundaryTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkPurchaseRespecReconcileTest
