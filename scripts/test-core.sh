2b040e2cda2ba7112f6d950c5e28aec1bd741a46
Gustavaopere <gustavaopere@gmail.com>
2026-08-25 14:56:48 -0300

    test: execute P-0021 provider boundary contract

--- scripts/test-core.sh
+++ scripts/test-core.sh
@@ -44,6 +44,7 @@ java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkPurchaseRespecReconc
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.FrozenA0005GateLifecycleTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.FrozenA0023PositionContractTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.FrozenCombatGatewayLifecycleTest
+java -cp "$OUT" dev.gustavopere.rpgskilltree.core.FrozenProviderBoundaryContractTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.FrozenA0010FuryPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.FrozenPhysicalProtectionPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.FrozenA0046FocusPolicyTest
