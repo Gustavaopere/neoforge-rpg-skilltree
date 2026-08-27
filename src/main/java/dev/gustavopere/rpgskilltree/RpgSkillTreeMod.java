220b257ae75611eea350c43927b3ce890d487328
Gustavaopere <gustavaopere@gmail.com>
2026-08-27 13:42:55 -0300

    feat: register Core progression rules reloader

--- src/main/java/dev/gustavopere/rpgskilltree/RpgSkillTreeMod.java
+++ src/main/java/dev/gustavopere/rpgskilltree/RpgSkillTreeMod.java
@@ -14,6 +14,7 @@ import dev.gustavopere.rpgskilltree.runtime.data.ArchetypeReloader;
 import dev.gustavopere.rpgskilltree.runtime.data.BossRewardReloader;
 import dev.gustavopere.rpgskilltree.runtime.data.ClassChoiceRulesReloader;
 import dev.gustavopere.rpgskilltree.runtime.data.ClassRulesReloader;
+import dev.gustavopere.rpgskilltree.runtime.data.CoreProgressionRulesReloader;
 import dev.gustavopere.rpgskilltree.runtime.data.NodeEffectsReloader;
 import dev.gustavopere.rpgskilltree.runtime.data.NodeRulesReloader;
 import dev.gustavopere.rpgskilltree.runtime.data.SpecializationReloader;
@@ -49,6 +50,7 @@ public final class RpgSkillTreeMod {
         NeoForge.EVENT_BUS.register(MorphCategoryReloader.class);
         NeoForge.EVENT_BUS.register(NodeEffectsReloader.class);
         NeoForge.EVENT_BUS.register(BossRewardReloader.class);
+        NeoForge.EVENT_BUS.register(CoreProgressionRulesReloader.class);
         NeoForge.EVENT_BUS.register(ApothicBossBridgeEvents.class);
         NeoForge.EVENT_BUS.register(BossProgressionEvents.class);
         NeoForge.EVENT_BUS.register(CombatProgressionEvents.class);
