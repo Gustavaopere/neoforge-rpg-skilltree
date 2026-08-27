904b0bc024fdde3a232944a5d1e79f021a60bba6
Gustavaopere <gustavaopere@gmail.com>
2026-08-27 15:35:23 -0300

    test: run level core point award policy contract

--- scripts/test-core.sh
+++ scripts/test-core.sh
@@ -47,3 +47,4 @@ java -cp "$OUT" dev.gustavopere.rpgskilltree.core.AttributeRanksPersistenceTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.AttributeRankMutationServiceTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.AttributeRankCostPolicyProviderTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.UnitAttributeRankCostPolicyTest
+java -cp "$OUT" dev.gustavopere.rpgskilltree.core.LevelCorePointAwardPolicyTest
