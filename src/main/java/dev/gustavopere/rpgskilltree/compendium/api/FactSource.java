5cc79fa2467b6d3506c0856cbfb90c233c10d5c2
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 14:10:30 -0300

    feat: add compendium fact provenance enums

---
+++ src/main/java/dev/gustavopere/rpgskilltree/compendium/api/FactSource.java
@@ -0,0 +1,13 @@
+package dev.gustavopere.rpgskilltree.compendium.api;
+
+public enum FactSource {
+    REGISTRY,
+    RUNTIME_ENTITY,
+    LOOT_TABLE,
+    DATAPACK,
+    RESOURCE_PACK,
+    ADAPTER,
+    CURATED_EDITORIAL,
+    MOD_LOCALIZATION,
+    UNKNOWN
+}
