2f2b86ffaf3ccf91bc920211f3f0ee21cb113d8b
Gustavaopere <gustavaopere@gmail.com>
2026-08-27 23:00:35 -0300

    test: execute entity scaling persistence contract

--- scripts/test-core.sh
+++ scripts/test-core.sh
@@ -58,4 +58,5 @@ java -cp "$OUT" dev.gustavopere.rpgskilltree.core.NativeAreaThreatCompositionTes
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.RelevantPlayerLevelFoundationTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.EntityArchetypeStatScalingTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.MobRarityFoundationTest
+java -cp "$OUT" dev.gustavopere.rpgskilltree.core.EntityScalingPersistenceTest
 python3 "$ROOT/scripts/verify-quest-runtime.py"
