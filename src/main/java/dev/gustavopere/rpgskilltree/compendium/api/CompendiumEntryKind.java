7c964e671085e3510dca7f36258236a9c825e951
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 14:10:07 -0300

    feat: add canonical compendium identity types

---
+++ src/main/java/dev/gustavopere/rpgskilltree/compendium/api/CompendiumEntryKind.java
@@ -0,0 +1,12 @@
+package dev.gustavopere.rpgskilltree.compendium.api;
+
+public enum CompendiumEntryKind {
+    ENTITY,
+    FLORA,
+    TREE,
+    CROP,
+    BIOME,
+    STRUCTURE,
+    DIMENSION,
+    BLOCK_FEATURE
+}
