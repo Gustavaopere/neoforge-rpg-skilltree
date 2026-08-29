40a694997846d7074a92742d491241e42bbb757c
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 21:15:14 -0300

    test: execute loot summary contract

--- scripts/compendium/test_ecology.sh
+++ scripts/compendium/test_ecology.sh
@@ -4,6 +4,8 @@ ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 cd "$ROOT"
 
 bash scripts/compendium/test_model_catalog.sh
+OUT="$ROOT/build/compendium-model-test-classes"
+java -cp "$OUT" dev.gustavopere.rpgskilltree.compendium.loot.LootSummaryTest
 
 if [[ -f scripts/compendium/verify_ecology_runtime.py ]]; then
   python3 scripts/compendium/verify_ecology_runtime.py
