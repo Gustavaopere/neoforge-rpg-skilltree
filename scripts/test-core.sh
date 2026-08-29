5445d9a6ead5fbbe47c1f1b5df93fe5afb61a7ba
Gustavaopere <gustavaopere@gmail.com>
2026-08-29 06:59:33 -0300

    test: execute A0041-A0060 behavior contract

--- scripts/test-core.sh
+++ scripts/test-core.sh
@@ -95,6 +95,7 @@ java -cp "$OUT" dev.gustavopere.rpgskilltree.core.A0021A0040CombatPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.A0021A0040MasteryPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.A0021A0040RespecReconcileTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.A0041A0060NotionContractTest
+java -cp "$OUT" dev.gustavopere.rpgskilltree.core.A0041A0060CombatPolicyTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.core.CombatPerkGatewayContractTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.runtime.data.NodeEffectCatalogReloadTest
 python3 "$ROOT/scripts/verify-1211-resources.py"
