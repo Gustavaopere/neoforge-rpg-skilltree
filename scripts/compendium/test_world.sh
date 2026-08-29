bb2673765e538b855d33ee5d72be04e4d0669311
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 23:39:00 -0300

    test: require world reload and structure discovery

--- scripts/compendium/test_world.sh
+++ scripts/compendium/test_world.sh
@@ -8,6 +8,9 @@ OUT="$ROOT/build/compendium-model-test-classes"
 java -cp "$OUT" dev.gustavopere.rpgskilltree.compendium.world.BiomeProviderTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.compendium.world.StructureProviderTest
 java -cp "$OUT" dev.gustavopere.rpgskilltree.compendium.world.DimensionProviderTest
+java -cp "$OUT" dev.gustavopere.rpgskilltree.compendium.world.WorldDiscoveryTest
 
 python3 scripts/compendium/verify_world_runtime.py
 python3 scripts/compendium/verify_world_catalog_publication.py
+python3 scripts/compendium/verify_world_reload.py
+python3 scripts/compendium/verify_world_discovery.py
