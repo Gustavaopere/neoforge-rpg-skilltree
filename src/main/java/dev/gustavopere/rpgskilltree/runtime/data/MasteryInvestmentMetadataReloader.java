bc78af5314c14b6c2f4063e9706bd6b3828b6e66
Gustavaopere <gustavaopere@gmail.com>
2026-09-05 19:35:20 -0300

    feat(mastery): register metadata reloader on game bus

--- src/main/java/dev/gustavopere/rpgskilltree/runtime/data/MasteryInvestmentMetadataReloader.java
+++ src/main/java/dev/gustavopere/rpgskilltree/runtime/data/MasteryInvestmentMetadataReloader.java
@@ -3,16 +3,19 @@ package dev.gustavopere.rpgskilltree.runtime.data;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import dev.gustavopere.rpgskilltree.RpgSkillTreeMod;
 import java.util.Map;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.AddReloadListenerEvent;
 import org.jetbrains.annotations.NotNull;
 
 /** Loads the explicit datapack source for Mastery contributions to emergent class investment. */
+@EventBusSubscriber(modid = RpgSkillTreeMod.MOD_ID)
 public final class MasteryInvestmentMetadataReloader extends SimpleJsonResourceReloadListener {
     private static final Gson GSON = new GsonBuilder().create();
 
