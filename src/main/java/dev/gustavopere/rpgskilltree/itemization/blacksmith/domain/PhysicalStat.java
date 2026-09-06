a4d92cc799c5b1958876347ee044603d5f83e325
Gustavaopere <gustavaopere@gmail.com>
2026-09-06 10:29:27 -0300

    feat(blacksmith): implement foundation domain

---
+++ src/main/java/dev/gustavopere/rpgskilltree/itemization/blacksmith/domain/PhysicalStat.java
@@ -0,0 +1,14 @@
+package dev.gustavopere.rpgskilltree.itemization.blacksmith.domain;
+
+/** Physical/statistical dimensions contributed by material and part composition. */
+public enum PhysicalStat {
+    DURABILITY,
+    ATTACK_DAMAGE,
+    ATTACK_SPEED,
+    MINING_SPEED,
+    ARMOR,
+    ARMOR_TOUGHNESS,
+    KNOCKBACK_RESISTANCE,
+    ENCHANTABILITY,
+    MASS
+}
