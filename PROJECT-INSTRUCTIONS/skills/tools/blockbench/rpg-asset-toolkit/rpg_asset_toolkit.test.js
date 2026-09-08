7a26a01dde5e9169008ff517179a9cf38ba55592
Gustavaopere <gustavaopere@gmail.com>
2026-09-08 20:59:51 -0300

    test(blockbench): define authenticated MCP sidecar contract

--- PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
+++ PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
@@ -41,3 +41,4 @@ require('./rpg_asset_toolkit.registry.test.js');
 require('./rpg_asset_toolkit.modular.test.js');
 require('./rpg_asset_toolkit.provider_expansion.test.js');
 require('./rpg_asset_toolkit.live_bridge.test.js');
+require('./rpg_asset_toolkit.mcp_sidecar.test.js');
