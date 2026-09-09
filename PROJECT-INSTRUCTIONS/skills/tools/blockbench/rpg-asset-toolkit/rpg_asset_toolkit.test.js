fb6097916417546dd116e6fcce7131e68a64c725
Gustavaopere <gustavaopere@gmail.com>
2026-09-09 14:15:13 -0300

    test(blockbench): include UV pack contract in aggregate

--- PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
+++ PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
@@ -51,3 +51,4 @@ require('./rpg_asset_toolkit.uv_texture_adapter.test.js');
 require('./rpg_asset_toolkit.uv_texture.test.js');
 require('./rpg_asset_toolkit.uv_analysis.test.js');
 require('./rpg_asset_toolkit.texture_path.test.js');
+require('./rpg_asset_toolkit.uv_pack.test.js');
