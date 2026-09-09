3508c325dbf51b8250ffa91a1289235c80596736
Gustavaopere <gustavaopere@gmail.com>
2026-09-08 23:04:22 -0300

    test(blockbench): cover native locator position semantics

--- PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
+++ PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
@@ -45,3 +45,4 @@ require('./rpg_asset_toolkit.mcp_sidecar.test.js');
 require('./rpg_asset_toolkit.bundle_live_bridge.test.js');
 require('./rpg_asset_toolkit.blockbench_adapter.test.js');
 require('./rpg_asset_toolkit.mutations.test.js');
+require('./rpg_asset_toolkit.locator_native.test.js');
