766265fc39aae14c0c2d9bd9f1c99f26a2a2727e
Gustavaopere <gustavaopere@gmail.com>
2026-09-09 18:09:39 -0300

    test(blockbench): include texture create import contract

--- PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
+++ PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
@@ -52,3 +52,4 @@ require('./rpg_asset_toolkit.uv_texture.test.js');
 require('./rpg_asset_toolkit.uv_analysis.test.js');
 require('./rpg_asset_toolkit.texture_path.test.js');
 require('./rpg_asset_toolkit.uv_pack.test.js');
+require('./rpg_asset_toolkit.texture_create_import.test.js');
