da1b6cdd82dc3c8d344a80ebb4df61b350a4af52
Gustavaopere <gustavaopere@gmail.com>
2026-09-09 13:34:04 -0300

    test(blockbench): include texture path contract in aggregate

--- PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
+++ PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
@@ -50,3 +50,4 @@ require('./rpg_asset_toolkit.uv_texture_revision.test.js');
 require('./rpg_asset_toolkit.uv_texture_adapter.test.js');
 require('./rpg_asset_toolkit.uv_texture.test.js');
 require('./rpg_asset_toolkit.uv_analysis.test.js');
+require('./rpg_asset_toolkit.texture_path.test.js');
