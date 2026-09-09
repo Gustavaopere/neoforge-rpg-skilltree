3e44cf8b4743eb42d3001701192d8efae804ee52
Gustavaopere <gustavaopere@gmail.com>
2026-09-09 00:57:59 -0300

    test(blockbench): load UV texture revision regression

--- PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
+++ PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
@@ -46,4 +46,5 @@ require('./rpg_asset_toolkit.bundle_live_bridge.test.js');
 require('./rpg_asset_toolkit.blockbench_adapter.test.js');
 require('./rpg_asset_toolkit.mutations.test.js');
 require('./rpg_asset_toolkit.locator_native.test.js');
+require('./rpg_asset_toolkit.uv_texture_revision.test.js');
 require('./rpg_asset_toolkit.uv_texture.test.js');
