61d028956b2c0c2f425694783586e5587c5a9ade
Gustavaopere <gustavaopere@gmail.com>
2026-09-09 22:35:49 -0300

    test(blockbench): include PR4 texture acceptance RED

--- PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
+++ PROJECT-INSTRUCTIONS/skills/tools/blockbench/rpg-asset-toolkit/rpg_asset_toolkit.test.js
@@ -57,3 +57,4 @@ require('./rpg_asset_toolkit.texture_create_import_adapter.test.js');
 require('./rpg_asset_toolkit.texture_paint_region.test.js');
 require('./rpg_asset_toolkit.uv_island_masks.test.js');
 require('./rpg_asset_toolkit.palette_sampling.test.js');
+require('./rpg_asset_toolkit.texture_acceptance.test.js');
